904c56fded7ac1318773aed6f30e18c2cb9e9086
Michael Bock <no-day@posteo.net>
2026-05-16 16:27:37 +0200

    Wip

--- app/Main.hs
+++ app/Main.hs
@@ -3,7 +3,7 @@
 {-# LANGUAGE NamedFieldPuns #-}
 module Main where
 
-import OpenSCAD.Model (Model(..), Solid(..), Facet(..), V3, render)
+import OpenSCAD.Model (Model(..), Facet(..), V3, render)
 
 
 pitch :: Double
@@ -21,21 +21,21 @@ diameter = 3
 clearance :: Double
 clearance = 0.2
 
-s1 :: Model V3
+s1 :: Model
 s1 = Union
-  [ Translate (- (width / 2), 0, 0) $ Solid (Cube { size = (width, pitch, baseHeight) })
+  [ Translate (- (width / 2), 0, 0) $ Cube { size = (width, pitch, baseHeight) }
   , Translate (0, pitch - r, 0)
-     $ Solid (Cylinder { height = 10, radius1 = r, radius2 = r, facet = NumFacets 30 })
+     $ Cylinder { height = 10, radius1 = r, radius2 = r, facet = NumFacets 30 }
   ]
   where
     r = (diameter / 2) - clearance
 
-s2 :: Model V3
+s2 :: Model
 s2 = Union $ fmap (\i -> Translate (0, i * pitch, 0) s1) [0..5]
 
 
 
-sketch :: Model V3
+sketch :: Model
 sketch = Union
  [ Translate (-40, 0, 0) s2
  , Translate (40, 0, 0) s2
--- app/OpenSCAD/Model.hs
+++ app/OpenSCAD/Model.hs
@@ -9,38 +9,39 @@ import qualified OpenSCAD.Raw as Raw
 data Facet = MinAngle Double | MinSize Double | NumFacets Int | Def
   deriving Show
 
-data Shape = Rectangle Double Double
-             -- | Circle Double Facet
-             -- | Polygon Int [Vector2d] [[Int]]
-             -- | Projection Bool Model3d
-             -- | Offset Double Join Shape
-           deriving Show
+-- data Shape = Rectangle Double Double
+--              -- | Circle Double Facet
+--              -- | Polygon Int [Vector2d] [[Int]]
+--              -- | Projection Bool Model3d
+--              -- | Offset Double Join Shape
+--            deriving Show
 
-data Solid -- Sphere Double --Facet
-           = Cube { size :: (Double, Double, Double) }
-           | Cylinder { height :: Double, radius1 :: Double, radius2 :: Double, facet :: Facet }
-        --    | ObCylinder Double Double Double Facet
-        --    | Polyhedron Int [Vector3d] Sides
-        --    | MultMatrix TransMatrix Model3d
-        --    | LinearExtrude Double Double Vector2d Int Int Facet Model2d
-        --    | RotateExtrude Int Facet Model2d
-        --    | Surface FilePath Bool Int
-        --    | ToSolid Model2d
-           deriving Show
+-- data Solid -- Sphere Double --Facet
+--            = Cube { size :: (Double, Double, Double) }
+--            | Cylinder { height :: Double, radius1 :: Double, radius2 :: Double, facet :: Facet }
+--         --    | ObCylinder Double Double Double Facet
+--         --    | Polyhedron Int [Vector3d] Sides
+--         --    | MultMatrix TransMatrix Model3d
+--         --    | LinearExtrude Double Double Vector2d Int Int Facet Model2d
+--         --    | RotateExtrude Int Facet Model2d
+--         --    | Surface FilePath Bool Int
+--         --    | ToSolid Model2d
+--            deriving Show
 
 
 
-data Model v = Solid Solid
+data Model  = Cube { size :: (Double, Double, Double) }
+            | Cylinder { height :: Double, radius1 :: Double, radius2 :: Double, facet :: Facet }
             --  | Shape Shape
             --  | Scale v (Model v)
             --  | Resize v (Model v)
             --  | Rotate v (Model v)
-            | Translate v (Model v)
+            | Translate V3 Model
             --  | Mirror v (Model v)
             --  | Color (Colour Double) (Model v)
             --  | Transparent (AlphaColour Double) (Model v)
             --  -- and combinations
-            | Union [Model v]
+            | Union [Model]
             --  | Intersection [Model v]
             --  | Minkowski [Model v]
             --  | Hull [Model v]
@@ -50,40 +51,25 @@ data Model v = Solid Solid
             --  | Var Facet [Model v]
             --  deriving Show
 
-class Vector v
-  where
-    toVec :: v -> [Double]
+toRawVec :: V3 -> Raw.Ast
+toRawVec (x, y, z) = Vec [LitDouble x, LitDouble y, LitDouble z]
 
-instance Vector (Double, Double, Double) where
-  toVec (x, y, z) = [x, y, z]
-
-instance Vector (Double, Double) where
-  toVec (x, y) = [x, y]
-
-toRawVec :: Vector v => v -> Raw.Ast
-toRawVec v = Vec (fmap LitDouble (toVec v))
-
-toRaw :: Vector v => Model v -> Raw.Ast
+toRaw :: Model -> Raw.Ast
 toRaw = \case
-  --Shape2d shape2d -> rShape2d shape2d
-  Solid solid       -> rSolid solid
+  Cube { size = (x, y, z) }
+    -> App
+         "cube"
+         [(Just "size", Vec [LitDouble x, LitDouble y, LitDouble z])]
+         []
+  Cylinder { height, radius1, radius2, facet }
+    -> App
+         "cylinder"
+         ([(Just "h", LitDouble height), (Just "r1", LitDouble radius1), (Just "r2", LitDouble radius2)] ++ renderFacet facet)
+         []
   Union models      -> App "union" [] (fmap toRaw models)
   Translate v model -> App "translate" [(Just "v", toRawVec v)] [toRaw model]
-  where
-    rSolid :: Solid -> Raw.Ast
-    rSolid = \case
-      Cube { size = (x, y, z) }
-        -> App
-             "cube"
-             [(Just "size", Vec [LitDouble x, LitDouble y, LitDouble z])]
-             []
-      Cylinder { height, radius1, radius2, facet }
-        -> App
-             "cylinder"
-             ([(Just "h", LitDouble height), (Just "r1", LitDouble radius1), (Just "r2", LitDouble radius2)] ++ renderFacet facet)
-             []
 
-render :: Vector v => Model v -> String
+render :: Model -> String
 render = Raw.render . toRaw
 
 renderFacet :: Facet -> [ (Maybe String, Raw.Ast) ]
