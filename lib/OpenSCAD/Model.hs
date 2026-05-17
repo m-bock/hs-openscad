9914c49386ced2a521d07707aa95c0d65be56b16
Michael Bock <no-day@posteo.net>
2026-05-17 15:45:11 +0200

    Refactor

--- lib/OpenSCAD/Model.hs
+++ lib/OpenSCAD/Model.hs
@@ -1,11 +1,15 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Redundant bracket" #-}
 {-# HLINT ignore "Use :" #-}
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+{-# HLINT ignore "Use ++" #-}
+{-# HLINT ignore "Evaluate" #-}
 
 module OpenSCAD.Model (Model3D(..), Model2D(..), Facets(..), V3, V2, RGB, render) where
 
 import OpenSCAD.Raw (Ast(..))
 import qualified OpenSCAD.Raw as Raw
+import Data.Maybe (catMaybes)
 
 data Facets = Facets {
   fa :: Maybe Double,
@@ -101,164 +105,260 @@ data Transform3D
 --- ToRaw
 -------------------------------------------------------------------------------
 
+optional :: (a -> b) -> Maybe a -> [b]
+optional f = maybe [] (pure . f)
+
+optionals :: (a -> [b]) -> Maybe a -> [b]
+optionals f = maybe [] f
+
+required :: a -> [a]
+required x = [x]
+
 toRawModel2D :: Model2D -> Raw.Ast
 toRawModel2D = \case
   Primitive2D (Circle2D { d, _facets })
     -> App "circle"
-         ([(Just "d", LitDouble d)] ++ toRawFacets _facets)
+         (concat
+           [ required (Just "d", LitDouble d)
+           , optionals toRawFacets _facets
+           ]
+         )
          []
   Primitive2D (Square2D { size, center })
     -> App "square"
-         ([(Just "size", toRawVec2Double size)] ++ maybe [] (\c -> [(Just "center", LitBool c)]) center)
+         (concat
+           [ required (Just "size", toRawVec2Double size)
+           , optional (\c -> (Just "center", LitBool c)) center
+           ]
+         )
          []
+  Primitive2D (Polygon2D { points, paths, convexity })
+    -> App "polygon"
+         (concat
+           [ required (Just "points", Vec $ map toRawVec2Double points)
+           , optional (\p -> (Just "paths", Vec $ map (Vec . map LitInt) p)) paths
+           , optional (\c -> (Just "convexity", LitInt c)) convexity
+           ]
+         )
+         []
+  Transform2D (Scale2D { v }) children
+    -> App "scale"
+         (concat
+           [ required (Just "v", toRawVec2Double v)
+           ]
+         )
+         (map toRawModel2D children)
+  Transform2D (Resize2D { v, auto }) children
+    -> App "resize"
+         (concat
+           [ required (Just "v", toRawVec2Double v)
+           , optional (\(a1, a2) -> (Just "auto", Vec [LitBool a1, LitBool a2])) auto
+           ]
+         )
+         (map toRawModel2D children)
+  Transform2D (RotateEuler2D { v }) children
+    -> App "rotate_euler"
+         (concat
+           [ required (Just "v", toRawVec2Double v)
+           ]
+         )
+         (map toRawModel2D children)
+  Transform2D (RotateAxis2D { a }) children
+    -> App "rotate_axis"
+         (concat
+           [ required (Just "a", LitDouble a)
+           ]
+         )
+         (map toRawModel2D children)
+  Transform2D (Translate2D { v }) children
+    -> App "translate"
+         (concat
+           [ required (Just "v", toRawVec2Double v)
+           ]
+         )
+         (map toRawModel2D children)
+  Transform2D (Mirror2D { v }) children
+    -> App "mirror"
+         (concat
+           [ required (Just "v", toRawVec2Double v)
+           ]
+         )
+         (map toRawModel2D children)
+  Transform2D (Color2D { c, alpha }) children
+    -> App "color"
+         (concat
+           [ required (Just "c", toRawVec3Double c)
+           , optional (\a -> (Just "alpha", LitDouble a)) alpha
+           ]
+         )
+         (map toRawModel2D children)
+  Transform2D (OffsetRadial2D { r }) children
+    -> App "offset_radial"
+         (concat
+           [ required (Just "r", LitDouble r)
+           ]
+         )
+         (map toRawModel2D children)
+  Transform2D (OffsetDelta2D { delta, chamfer }) children
+    -> App "offset_delta"
+         (concat
+           [ required (Just "delta", LitDouble delta)
+           , optional (\c -> (Just "chamfer", LitBool c)) chamfer
+           ]
+         )
+         (map toRawModel2D children)
+  Transform2D (Fill2D) children
+    -> App "fill"
+         []
+         (map toRawModel2D children)
+  Transform2D (Minkowski2D) children
+    -> App "minkowski"
+         []
+         (map toRawModel2D children)
+  Transform2D (Hull2D) children
+    -> App "hull"
+         []
+         (map toRawModel2D children)
+  Transform2D (Union2D) children
+    -> App "union"
+         []
+         (map toRawModel2D children)
+  Transform2D (Intersection2D) children
+    -> App "intersection"
+         []
+         (map toRawModel2D children)
+  Transform2D (Difference2D) children
+    -> App "difference"
+         []
+         (map toRawModel2D children)
 
 
---   Circle2D Circle2DSpec { d }
---     -> App "circle"
---          [(Just "d", LitDouble d)]
---          []
---   Rect2D (x, y)
---     -> App "square"
---          [(Nothing, Vec [LitDouble x, LitDouble y])]
---          []
+toRawModel3D :: Model3D -> Raw.Ast
+toRawModel3D = \case
+  Primitive3D (Cube3D { size })
+    -> App "cube"
+         (concat
+           [ required (Just "size", toRawVec3Double size)
+           ]
+         )
+         []
+  Primitive3D (Cylinder3D { h, d1, d2, _facets })
+    -> App "cylinder"
+         (concat
+           [ required (Just "h", LitDouble h)
+           , required (Just "d1", LitDouble d1)
+           , required (Just "d2", LitDouble d2)
+           , optionals toRawFacets _facets
+           ]
+         )
+         []
+  Primitive3D (Sphere3D { d, _facets })
+    -> App "sphere"
+         (concat
+           [ required (Just "d", LitDouble d)
+           , optionals toRawFacets _facets
+           ]
+         )
+         []
+  Primitive3D (Polyhedron3D { points, faces, convexity })
+    -> App "polyhedron"
+         (concat
+           [ required (Just "points", Vec $ map toRawVec3Double points)
+           , optional (\f -> (Just "faces", Vec $ map (Vec . map LitInt) f)) faces
+           , optional (\c -> (Just "convexity", LitInt c)) convexity
+           ]
+         )
+         []
+  Transform3D (Scale3D { v }) children
+    -> App "scale"
+         (concat
+           [ required (Just "v", toRawVec3Double v)
+           ]
+         )
+         (map toRawModel3D children)
+  Transform3D (Resize3D { v, auto }) children
+    -> App "resize"
+         (concat
+           [ required (Just "v", toRawVec3Double v)
+           , optional (\(a1, a2, a3) -> (Just "auto", Vec [LitBool a1, LitBool a2, LitBool a3])) auto
+           ]
+         )
+         (map toRawModel3D children)
+  Transform3D (RotateEuler3D { v }) children
+    -> App "rotate"
+         (concat
+           [ required (Just "v", toRawVec3Double v)
+           ]
+         )
+         (map toRawModel3D children)
+  Transform3D (RotateAxis3D { a, v }) children
+    -> App "rotate"
+         (concat
+           [ required (Just "a", LitDouble a)
+           , required (Just "v", toRawVec3Double v)
+           ]
+         )
+         (map toRawModel3D children)
+  Transform3D (Translate3D { v }) children
+    -> App "translate"
+         (concat
+           [ required (Just "v", toRawVec3Double v)
+           ]
+         )
+         (map toRawModel3D children)
+  Transform3D (Mirror3D { v }) children
+    -> App "mirror"
+         (concat
+           [ required (Just "v", toRawVec3Double v)
+           ]
+         )
+         (map toRawModel3D children)
+  Transform3D (Color3D { c, alpha }) children
+    -> App "color"
+         (concat
+           [ optional (\co -> (Just "c", toRawVec3Double co)) c
+           , optional (\a -> (Just "alpha", LitDouble a)) alpha
+           ]
+         )
+         (map toRawModel3D children)
+  Transform3D (Union3D) children
+    -> App "union"
+         []
+         (map toRawModel3D children)
+  Transform3D (Intersection3D) children
+    -> App "intersection"
+         []
+         (map toRawModel3D children)
+  Transform3D (Difference3D) children
+    -> App "difference"
+         []
+         (map toRawModel3D children)
+  Transform3D (Minkowski3D) children
+    -> App "minkowski"
+         []
+         (map toRawModel3D children)
+  Transform3D (Hull3D) children
+    -> App "hull"
+         []
+         (map toRawModel3D children)
 
---   Scale2D (x, y) model
---     -> App "scale"
---          [(Just "v", toRawVec2Double (x, y))]
---          [toRawModel2D model]
---   Resize2D v model
---     -> App "resize"
---          (toRawVec2MaybeDouble v)
---          [toRawModel2D model]
---   Rotate2D angle model
---     -> App "rotate"
---          [(Just "angle", LitDouble angle)]
---          [toRawModel2D model]
---   Translate2D v model
---     -> App "translate"
---          [(Just "v", toRawVec2Double v)]
---          [toRawModel2D model]
---   Mirror2D v model
---     -> App "mirror"
---          [(Just "v", toRawVec2Double v)]
---          [toRawModel2D model]
-
---   Color2D ColorSpec { c, alpha } model
---     -> App "color"
---          [(Just "c", toRawVec3Double c), (Just "alpha", LitDouble alpha)]
---          [toRawModel2D model]
-
--- toRawModel3D :: Model3D -> Raw.Ast
--- toRawModel3D = \case
---   Box3D (x, y, z)
---     -> App "cube"
---          [(Nothing, Vec [LitDouble x, LitDouble y, LitDouble z])]
---          []
---   Cone3D ConeSpec { h, d1, d2, facet }
---     -> App "cylinder"
---          ([(Just "h", LitDouble h), (Just "d1", LitDouble d1), (Just "d2", LitDouble d2)] 
---           ++ toRawFacet facet)
---          []
---   Sphere3D SphereSpec { d }
---     -> App "sphere"
---          [(Just "d", LitDouble d)]
---          []
-
---   Scale3D v model
---     -> App "scale"
---          [(Just "v", toRawVec3Double v)]
---          [toRawModel3D model]
---   Resize3D v model
---     -> App "resize"
---          (toRawVec3MaybeDouble v)
---          [toRawModel3D model]
---   RotateEuler3D v model
---     -> App "rotate_euler"
---          [(Just "v", toRawVec3Double v)]
---          [toRawModel3D model]
---   RotateAxis3D angle v model
---     -> App "rotate_axis"
---          [(Just "angle", LitDouble angle), (Just "v", toRawVec3Double v)]
---          [toRawModel3D model]
---   Translate3D v model
---     -> App "translate"
---          [(Just "v", toRawVec3Double v)]
---          [toRawModel3D model]
---   Mirror3D v model
---     -> App "mirror"
---          [(Just "v", toRawVec3Double v)]
---          [toRawModel3D model]
-
---   Color3D ColorSpec { c, alpha } model
---     -> App "color"
---          [(Just "c", toRawVec3Double c), (Just "alpha", LitDouble alpha)]
---          [toRawModel3D model]
-
---   Union3D models      
---     -> App "union"
---          []
---          (fmap toRawModel3D models)
---   Intersection3D models
---     -> App "intersection"
---          []
---          (fmap toRawModel3D models)
---   Difference3D model models
---     -> App "difference"
---          []
---          (toRawModel3D model : fmap toRawModel3D models)
-
---   Minkowski3D models
---     -> App "minkowski"
---          []
---          (fmap toRawModel3D models)
---   Hull3D models
---     -> App "hull"
---          []
---          (fmap toRawModel3D models)
-  
-
-toRawFacets :: Maybe Facets -> [ (Maybe String, Raw.Ast) ]
-toRawFacets = \case
-  Just Facets { fa, fs, fn } ->
+toRawFacets :: Facets -> [ (Maybe String, Raw.Ast) ]
+toRawFacets Facets { fa, fs, fn } =
     (maybe [] (\a -> [(Just "$fa", LitDouble a)]) fa) ++
     (maybe [] (\s -> [(Just "$fs", LitDouble s)]) fs) ++
     (maybe [] (\n -> [(Just "$fn", LitInt n)   ]) fn)
-  Nothing ->
-    []
 
 
--- toRawVec3Double :: V3 Double -> Raw.Ast
--- toRawVec3Double (x, y, z) = Vec [LitDouble x, LitDouble y, LitDouble z]
+toRawVec3Double :: V3 Double -> Raw.Ast
+toRawVec3Double (x, y, z) = Vec [LitDouble x, LitDouble y, LitDouble z]
 
 toRawVec2Double :: V2 Double -> Raw.Ast
 toRawVec2Double (x, y) = Vec [LitDouble x, LitDouble y]
 
--- toRawVec2MaybeDouble :: V2 (Maybe Double) -> [ (Maybe String, Raw.Ast) ]
--- toRawVec2MaybeDouble (x, y) =
---   [ ( Nothing
---     , Vec [ LitDouble (fromMaybe 0 x), LitDouble (fromMaybe 0 y) ]
---     )
---   , ( Just "auto"
---     , LitBool True
---     )
---   ]
 
--- toRawVec3MaybeDouble :: V3 (Maybe Double) -> [ (Maybe String, Raw.Ast) ]
--- toRawVec3MaybeDouble (x, y, z) =
---   [ ( Nothing
---     , Vec [ LitDouble (fromMaybe 0 x), LitDouble (fromMaybe 0 y), LitDouble (fromMaybe 0 z) ]
---     )
---   , ( Just "auto"
---     , LitBool True
---     )
---   ]
-
--- -------------------------------------------------------------------------------
--- --- Render
--- -------------------------------------------------------------------------------
-
--- render :: Model3D -> String
--- render = Raw.render . toRawModel3D
+-------------------------------------------------------------------------------
+--- Render
+-------------------------------------------------------------------------------
 
 render :: Model3D -> String
-render = undefined
+render = Raw.render . toRawModel3D
