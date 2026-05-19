8602a7d3c3376777d6ed1f60e4fee93faea3f821
Michael Bock <no-day@posteo.net>
2026-05-19 12:06:22 +0200

    Fix newline

--- lib/OpenSCAD/Raw.hs
+++ lib/OpenSCAD/Raw.hs
@@ -44,7 +44,7 @@ renderArg (name, arg) = case name of
 renderChildrenAt :: Int -> [Ast] -> String
 renderChildrenAt depth xs = case xs of
   [] -> ";"
-  [x] -> renderAt depth x
+  [x] -> "\n" ++ renderAt depth x
   xs -> "{" ++ (intercalate "" $ map (addNl . renderAt depth) xs) ++ "\n}"
 
 addNl :: String -> String
